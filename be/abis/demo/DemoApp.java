14f5990d0ee4d19a9487428da123ca4fa279cd41
Gie <gindesteege@abis.be>
2020-09-10 15:05:43 +0200

    second commit

--- be/abis/demo/DemoApp.java
+++ be/abis/demo/DemoApp.java
@@ -2,6 +2,6 @@ package be.abis.demo;
 
 public class DemoApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to the ABIS demo for Java tooling");
+        System.out.println("Welcome again to the ABIS demo for Java tooling");
     }
 }
