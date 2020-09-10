421366b509d5f47f7f44f516f617e5d9303b4cb5
Gie <gindesteege@abis.be>
2020-09-10 16:39:54 +0200

    adding person info

--- be/abis/demo/DemoApp.java
+++ be/abis/demo/DemoApp.java
@@ -3,5 +3,8 @@ package be.abis.demo;
 public class DemoApp {
     public static void main(String[] args) {
         System.out.println("Welcome again to the ABIS demo for Java tooling");
+		
+		Person p = new Person("James","Bond);
+		System.out.println("Hello " + p);
     }
 }
